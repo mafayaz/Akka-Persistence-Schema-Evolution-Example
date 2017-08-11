feaad795097b6d731227d19c2512f542bd795166
perdijn <Nathan.Perdijk@rabobank.nl>
2017-08-11 11:02:04 +0200

    added the JsonSerializerRegistry to the ActorSystemSetup, brought back to life and fixed the first version of the ShoppingCartSerialiserRegistry, added the formatting to the specification of the domain objects.

--- src/main/scala/com/experiments/calvin/Main.scala
+++ src/main/scala/com/experiments/calvin/Main.scala
@@ -1,18 +1,22 @@
 package com.experiments.calvin
 
 
+import akka.actor.setup.ActorSystemSetup
 import akka.actor.{ActorSystem, Props}
 import akka.pattern.ask
 import akka.util.Timeout
 import com.experiments.calvin.actors.ShoppingCartActor
 import com.experiments.calvin.actors.ShoppingCartActor.GetResult
 import com.experiments.calvin.models.{Item, ShoppingCart}
+import com.experiments.calvin.registry.ShoppingCartSerializerRegistry
+import com.lightbend.lagom.scaladsl.playjson.JsonSerializerRegistry
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
 object Main extends App {
+
   val shoppingCart = ShoppingCart(
     List(
       Item(1, "lays chips"),
@@ -27,8 +31,11 @@ object Main extends App {
 //    )
 //  )
 
+
   implicit val timeout = Timeout(10 seconds)
-  val actorSystem = ActorSystem(name = "example")
+
+  val actorSystem = ActorSystem(name = "example", ActorSystemSetup(JsonSerializerRegistry.serializationSetupFor(ShoppingCartSerializerRegistry)))
+
   val shoppingCartActor = actorSystem.actorOf(Props[ShoppingCartActor], name = "shopping-cart-actor")
   shoppingCartActor ! shoppingCart
   val futureResult = (shoppingCartActor ? GetResult).mapTo[ShoppingCart]
