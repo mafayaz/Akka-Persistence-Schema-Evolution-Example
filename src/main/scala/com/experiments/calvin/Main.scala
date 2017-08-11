01296f18e22924f4b8ccd1a8a11f8bf88fb1c7be
perdijn <Nathan.Perdijk@rabobank.nl>
2017-08-11 09:11:37 +0200

    Brought application to a state where it persists and recovers V3 events without upcasting and upcasts V2.

--- src/main/scala/com/experiments/calvin/Main.scala
+++ src/main/scala/com/experiments/calvin/Main.scala
@@ -6,32 +6,25 @@ import akka.pattern.ask
 import akka.util.Timeout
 import com.experiments.calvin.actors.ShoppingCartActor
 import com.experiments.calvin.actors.ShoppingCartActor.GetResult
-import com.experiments.calvin.models._
+import com.experiments.calvin.models.{ShoppingCartV3, ItemV3}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
 object Main extends App {
-
-  val shoppingCart = ShoppingCartV2(
+  val shoppingCart = ShoppingCartV3(
     List(
-      ItemV2(1, "lays chips", "cant have just one bite"),
-      ItemV2(2, "caramilk chocolate", "calories")
+      ItemV3(1, "lays chips", "cant have just one bite, or can I?"),
+      ItemV3(2, "caramilk chocolate", "calories")
     )
   )
-//  val shoppingCart = ShoppingCartV3(
-//    List(
-//      ItemV3(1, "lays chips", "cant have just one bite"),
-//      ItemV3(2, "caramilk chocolate", "calories")
-//    )
-//  )
 
   implicit val timeout = Timeout(10 seconds)
   val actorSystem = ActorSystem(name = "example")
   val shoppingCartActor = actorSystem.actorOf(Props[ShoppingCartActor], name = "shopping-cart-actor")
   shoppingCartActor ! shoppingCart
-  val futureResult = (shoppingCartActor ? GetResult).mapTo[ShoppingCartV2]
+  val futureResult = (shoppingCartActor ? GetResult).mapTo[ShoppingCartV3]
   val obtainedCart = Await.result(futureResult, 10 seconds)
   println(obtainedCart)
   actorSystem.terminate()
